97bce196c605587f7f4e02f4f44f6a9e3b238c6f
Kushal <Kushalgoyal168@gmail.com>
2024-05-06 00:08:39 +0530

    Added Basic conf

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,16 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
     # return "Hello world"
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+# @app.route("/edit",methods={"GET","POST"})
+# def edit():
+#     if(request.method == "POST"):
+#         print("reeeeeeqqqqq",request.form)
+#         return render_template("index.html")
 
 # app.run(debug = True,port=5001)
 if __name__ == "__main__":
