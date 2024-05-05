7ad71c0672bee2e5fc478d26e08047864229a52b
Kushal <Kushalgoyal168@gmail.com>
2024-05-06 00:52:14 +0530

    uploading filef to be converted in static folder

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask,render_template, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = 'static'
+ALLOWED_EXTENSIONS = {'pdf','png', 'webp', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def hello_world():
@@ -11,22 +21,28 @@ def hello_world():
 def about():
     return render_template("about.html")
 
-# @app.route("/edit",methods={"GET","POST"})
-# def edit():
-#     if(request.method == "POST"):
-#         print("reeeeeeqqqqq",request.form)
-#         return render_template("index.html")
 
 @app.route("/edit", methods=["GET", "POST"])
 def edit():
     if request.method == "POST":
         print("Form data:", request.form)
-        # Add your logic to handle the form data
-        return render_template("about.html")  # Example response
-    else:
-        # Add logic for GET requests if needed
-        return "Edit page"
+        if 'file' not in request.files:
+            flash('No file part')
+            return "error"
+        file = request.files['file']
+        print("_____",request.files ,"++++",file.filename)
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            print("=====>>>",file.filename)
+            flash('No selected file')
+            return "no fileeeee selected"
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # processImage()
+            return render_template("index.html")
+    return  render_template("index.html")
 
-# app.run(debug = True,port=5001)
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
